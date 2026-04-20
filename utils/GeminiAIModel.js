a826d99ddb7e64a8bf23127accfb36fcbc4fab2f
dhruvvv1104 <84135747+dhruvvv1104@users.noreply.github.com>
2026-04-20 23:01:41 +0530

    Change model from 'gemini-3.1-pro-preview' to 'gemini-3-flash-preview'

--- utils/GeminiAIModel.js
+++ utils/GeminiAIModel.js
@@ -8,7 +8,7 @@ const {
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
-    model: "gemini-3.1-pro-preview",
+    model: "gemini-3-flash-preview",
   });
   
   const generationConfig = {
