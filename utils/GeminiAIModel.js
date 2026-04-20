119797359de7a01f6c7ec8c552f70267e8dfb09d
dhruvvv1104 <84135747+dhruvvv1104@users.noreply.github.com>
2026-04-20 23:15:51 +0530

    Update GeminiAIModel.js

--- utils/GeminiAIModel.js
+++ utils/GeminiAIModel.js
@@ -8,7 +8,7 @@ const {
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
-    model: "gemini-2.5-pro",
+    model: "gemini-3.1-flash-lite-preview",
   });
   
   const generationConfig = {
